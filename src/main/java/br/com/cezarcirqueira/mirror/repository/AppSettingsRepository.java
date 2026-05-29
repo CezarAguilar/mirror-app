18ef7bbff516c10a3f14f21d84f05f718d734aac
cezar.cirqueira <aguilar.cac@gmail.com>
2026-05-29 15:15:17 -0300

    refactoring

--- src/main/java/br/com/cezarcirqueira/mirror/repository/AppSettingsRepository.java
+++
@@ -1,6 +0,0 @@
-package br.com.cezarcirqueira.mirror.repository;
-
-import br.com.cezarcirqueira.mirror.domain.AppSettings;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppSettingsRepository extends JpaRepository<AppSettings, Long> {}
